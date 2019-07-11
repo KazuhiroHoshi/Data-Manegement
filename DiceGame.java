67ab6afb9517c1a90a06863e46e2f82f5e32c3b6
KazuhiroHoshi <s1260080@u-aizu.ac.jp>
2019-07-11 11:43:26 +0900

    First revision

---
+++ DiceGame.java
@@ -0,0 +1,47 @@
+class Die{
+    private int faceValue;
+
+    public int getfaceValue(){
+	return faceValue;
+    }
+    public void roll(){
+	faceValue=(int)(Math.random()*6)+1;
+    }
+
+}
+
+class Player{
+    private String name;
+    public Player(String name){
+	this.name=name;
+    }
+    public String getName(){
+	return name;
+    }
+}
+
+public class DiceGame{
+    void play(){
+    Player  p=new Player("John");
+    Die die1=new Die();
+    Die die2=new Die();
+    System.out.println("Rolling the dice...");
+    die1.roll();
+    die2.roll();
+    System.out.println("Die 1: "+die1.getfaceValue());
+    System.out.println("Die 2: "+die2.getfaceValue());
+    System.out.println("Total Value : "+(die1.getfaceValue()+die2.getfaceValue()));
+
+}
+
+    public static void main(String args[]){
+	DiceGame dg = new DiceGame();
+	dg.play();
+
+    }
+}
+
+
+
+
+
